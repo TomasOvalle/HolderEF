156bfdda49408691b18fa559c05585dabce791ee
TomasOvalle <144947433+TomasOvalle@users.noreply.github.com>
2024-09-11 19:03:24 -0300

    Add files via upload

---
+++ navBar3.js
@@ -0,0 +1,15 @@
+async function printConditions3() {
+    try {
+        let response = await fetch("/api/sessions/online");
+        response = await response.json();
+
+        const user = response.response;
+
+        if (user.role === 2) {
+            template = `<li class="nav-item"> <a class="nav-link" href="./premProductsView.html">My products</a></li>`;
+            document.querySelector("#conditions3").innerHTML = template;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
